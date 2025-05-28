568a6388f2e63d2b722959ff01cd65fe3ea67763
Maria Jansson <mj225er@student.lnu.se>
2025-05-28 20:26:59 +0200

    Remove unused files

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-// Configuration file for Jest
-export default {
-  setupFiles: ['<rootDir>/src/test/setup/setupEnv.js', '<rootDir>/src/test/setup/jest.setup.mocks.js'],
-  testEnvironment: 'node'
-}
